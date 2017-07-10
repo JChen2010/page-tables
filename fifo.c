af332e9393fa93e5c88b14c361673bd5d5169b13
Boyang Li <bryanli2009@live.ca>
2017-07-10 12:41:21 -0400

    Implement FIFO eviction algorithm

--- fifo.c
+++ fifo.c
@@ -12,13 +12,19 @@ extern int debug;
 
 extern struct frame *coremap;
 
+// the oldest page in memory
+int oldest_idx;
+
 /* Page to evict is chosen using the fifo algorithm.
  * Returns the page frame number (which is also the index in the coremap)
  * for the page that is to be evicted.
  */
 int fifo_evict() {
-	
-	return 0;
+  // evict the current oldest page
+  int victim = oldest_idx;
+  // the oldest page is always the head of page addr list
+  oldest_idx = (oldest_idx + 1) % memsize;
+	return victim;
 }
 
 /* This function is called on each access to a page to update any information
@@ -30,8 +36,9 @@ void fifo_ref(pgtbl_entry_t *p) {
 	return;
 }
 
-/* Initialize any data structures needed for this 
- * replacement algorithm 
+/* Initialize any data structures needed for this
+ * replacement algorithm
  */
 void fifo_init() {
+  oldest_idx = 0;
 }
